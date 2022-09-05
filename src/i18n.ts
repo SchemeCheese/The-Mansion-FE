d541518625eb72bd9436f60df8ffaaf8e4de460f
iamar9 <thanhhang.hanu@gmail.com>
2022-09-05 23:27:55 +0700

    feat: pay selected + change disk + add pre audit room charge modal

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,19 @@
 import { initReactI18next } from 'react-i18next';
 import i18n from 'i18next';
+import auditRoomChargeEn from 'translations/en/auditRoomCharge.json';
 import commonEn from 'translations/en/common.json';
 import guestEn from 'translations/en/guest.json';
+import paySelectedEn from 'translations/en/paySelected.json';
 import reservationEn from 'translations/en/reservation.json';
+import auditRoomChargeJa from 'translations/ja/auditRoomCharge.json';
 import commonJa from 'translations/ja/common.json';
+import guestJa from 'translations/ja/guest.json';
+import paySelectedJa from 'translations/ja/paySelected.json';
 import reservationJa from 'translations/ja/reservation.json';
+import auditRoomChargeVi from 'translations/vi/auditRoomCharge.json';
 import commonVi from 'translations/vi/common.json';
+import guestVi from 'translations/vi/guest.json';
+import paySelectedVi from 'translations/vi/paySelected.json';
 import reservationVi from 'translations/vi/reservation.json';
 
 i18n.use(initReactI18next).init({
@@ -17,6 +25,8 @@ i18n.use(initReactI18next).init({
         common: commonEn,
         reservation: reservationEn,
         guest: guestEn,
+        paySelected: paySelectedEn,
+        auditRoomCharge: auditRoomChargeEn,
       },
     },
     ja: {
@@ -24,6 +34,9 @@ i18n.use(initReactI18next).init({
         'Welcome to React': 'Welcome to React and react-i18next',
         common: commonJa,
         reservation: reservationJa,
+        guest: guestJa,
+        paySelected: paySelectedJa,
+        auditRoomCharge: auditRoomChargeJa,
       },
     },
     vi: {
@@ -31,6 +44,9 @@ i18n.use(initReactI18next).init({
         'Welcome to React': 'Welcome to React and react-i18next',
         common: commonVi,
         reservation: reservationVi,
+        guest: guestVi,
+        paySelected: paySelectedVi,
+        auditRoomCharge: auditRoomChargeVi,
       },
     },
   },
