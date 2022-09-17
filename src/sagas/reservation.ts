54b3520b8fa4f806b34ed84034c80810669380e9
minhnv2306 <minhnv2306@gmail.com>
2022-09-17 15:09:27 +0700

    feat: send room data for create reservation api

--- src/sagas/reservation.ts
+++ src/sagas/reservation.ts
@@ -50,54 +50,6 @@ export function* postCreateReservationSaga({ payload }: ReturnType<typeof create
       operator_code: 'the_mansion',
       facility_code: 'hotel',
       reservation_number: '6666',
-      rooms: [
-        {
-          room_id: 1,
-          room_type: 2,
-          checkin_date: '2022-02-02',
-          checkout_date: '2022-02-03',
-          actual_amount: 4500000,
-          charges: [
-            {
-              equipment_type_id: 2,
-              charge_kind: 1,
-              actual_amount: 1000000,
-              use_date: '2022-02-02',
-              paid_up: 0,
-            },
-            {
-              equipment_type_id: 2,
-              charge_kind: 1,
-              actual_amount: 1000000,
-              use_date: '2022-02-03',
-              paid_up: 0,
-            },
-          ],
-        },
-        {
-          room_id: 2,
-          room_type: 2,
-          checkin_date: '2022-02-04',
-          checkout_date: '2022-02-05',
-          actual_amount: 4800000,
-          charges: [
-            {
-              equipment_type_id: 2,
-              charge_kind: 1,
-              actual_amount: 1500000,
-              use_date: '2022-02-04',
-              paid_up: 0,
-            },
-            {
-              equipment_type_id: 2,
-              charge_kind: 1,
-              actual_amount: 1500000,
-              use_date: '2022-02-05',
-              paid_up: 0,
-            },
-          ],
-        },
-      ],
     },
   }));
 
