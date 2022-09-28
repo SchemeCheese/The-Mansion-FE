bca2a1d291f133948ed6a2555baa94ef6700220c
Minh. Nguyen Van - CMC Japan DJ2 <nvminh2@cmcglobal.vn>
2022-09-28 16:32:28 +0700

    fix: fix cancel modal fe

--- src/pages/reservation/modal/CancelBookingModal.tsx
+++ src/pages/reservation/modal/CancelBookingModal.tsx
@@ -42,63 +42,77 @@ function CancelBookingModal({ isModalVisible, setModalVisible }: Props) {
       visible={isModalVisible}
       width={850}
     >
-      <Row>
-        <Col span={8}>
-          <Form.Item label={t('reservation.Folio ID')}>
-            <Input defaultValue="2808" disabled value="2000" />
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <div
-            style={{
-              bottom: 0,
-              position: 'absolute',
-              paddingBottom: 24,
-              textAlign: 'center',
-              paddingLeft: 34,
-            }}
-          >
-            <span>{t('reservation.Receptionist')}: Nguyen Dac Trung</span>
-          </div>
-        </Col>
-        <Col span={8}>
-          <Form.Item
-            name="remember"
-            style={{
-              marginBottom: 12,
-              bottom: 0,
-              position: 'absolute',
-              paddingBottom: 9,
-              width: '100%',
-              textAlign: 'right',
-            }}
-            valuePropName="checked"
-          >
-            <Checkbox>{t('reservation.Send confirmation email')}</Checkbox>
-          </Form.Item>
-        </Col>
-        <Col span={8}>
-          <Form.Item label={t('reservation.Type.title')} name="type">
-            <Select allowClear placeholder={t('reservation.Type.placeholder')}>
-              <Option value="room1">Room 1</Option>
-              <Option value="room2">Room 2</Option>
-              <Option value="room3">Room 3</Option>
-            </Select>
-          </Form.Item>
-        </Col>
-        <Col span={24}>
-          <Form.Item
-            label={t('reservation.Reason for booking cancellation (Optional).title')}
-            name="type"
-            wrapperCol={{ span: 24 }}
-          >
-            <TextArea
-              placeholder={t('reservation.Reason for booking cancellation (Optional).placeholder')}
-              rows={5}
-            />
-          </Form.Item>
-        </Col>
-      </Row>
+      <Form
+        autoComplete="off"
+        labelCol={{
+          span: 24,
+        }}
+        layout="vertical"
+        name="basic"
+        wrapperCol={{
+          span: 23,
+        }}
+      >
+        <Row>
+          <Col span={8}>
+            <Form.Item label={t('reservation.Folio ID')}>
+              <Input defaultValue="2808" disabled value="2000" />
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <div
+              style={{
+                bottom: 0,
+                position: 'absolute',
+                paddingBottom: 24,
+                textAlign: 'center',
+                paddingLeft: 34,
+              }}
+            >
+              <span>{t('reservation.Receptionist')}: Nguyen Dac Trung</span>
+            </div>
+          </Col>
+          <Col span={8}>
+            <Form.Item
+              name="remember"
+              style={{
+                marginBottom: 12,
+                bottom: 0,
+                position: 'absolute',
+                paddingBottom: 9,
+                width: '100%',
+                textAlign: 'right',
+              }}
+              valuePropName="checked"
+            >
+              <Checkbox>{t('reservation.Send confirmation email')}</Checkbox>
+            </Form.Item>
+          </Col>
+          <Col span={8}>
+            <Form.Item label={t('reservation.Type.title')} name="type">
+              <Select allowClear placeholder={t('reservation.Type.placeholder')}>
+                <Option value="room1">Room 1</Option>
+                <Option value="room2">Room 2</Option>
+                <Option value="room3">Room 3</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={24}>
+            <Form.Item
+              label={t('reservation.Reason for booking cancellation (Optional).title')}
+              name="type"
+              wrapperCol={{ span: 24 }}
+            >
+              <TextArea
+                placeholder={t(
+                  'reservation.Reason for booking cancellation (Optional).placeholder',
+                )}
+                rows={5}
+              />
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
     </Modal>
   );
 }
