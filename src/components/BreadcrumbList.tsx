4d2b91f11b265187a9d2dafc891c6acd106fd9d9
minhnv2306 <minhnv2306@gmail.com>
2022-11-19 17:35:01 +0700

    fix: fix breadcrum

--- src/components/BreadcrumbList.tsx
+++ src/components/BreadcrumbList.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Breadcrumb } from 'antd';
 
 interface Props {
-  data: string[];
+  data?: string[];
 }
 
 function BreadcrumbList({ data }: Props) {
   return (
     <Breadcrumb className="breadcrumb">
-      {data.map((item: string, index: number) => {
+      {data?.map((item: string, index: number) => {
         return <Breadcrumb.Item key={index}>{item}</Breadcrumb.Item>;
       })}
     </Breadcrumb>
