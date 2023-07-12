4af150d5d1a8320b17ca05290abb11d6a4886378
minhnv2306 <minhnv2306@gmail.com>
2023-07-12 21:53:30 +0700

    refactor: add loading for report

--- src/pages/report/list/HotelFacility.tsx
+++ src/pages/report/list/HotelFacility.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import { Card, Col, DatePicker, Form, Row, Select } from 'antd';
+import { Card, Col, DatePicker, Form, Row, Select, Spin } from 'antd';
 import { headerWithAuthorization } from 'helpers';
 import { getAPI } from 'helpers/apiService';
 import moment from 'moment';
@@ -24,6 +24,7 @@ function HotelFacility() {
 
   const [facilities, setFacilities] = useState([]);
   const [branchs, setBranchs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onReportFormFinish = async (values: any) => {
     const newValues = {
@@ -38,10 +39,13 @@ function HotelFacility() {
 
     const query = new URLSearchParams(Object(newValues)).toString();
 
+    setIsLoading(true);
+
     fetch(`${process.env.REACT_APP_API_HOST}/api/v1/reports/${values.report_type}?${query}`, {
       method: 'GET',
       headers: headerWithAuthorization(),
     }).then(response => {
+      setIsLoading(false);
       response.blob().then(blob => {
         const url = window.URL.createObjectURL(blob);
         const a = document.createElement('a');
@@ -106,129 +110,131 @@ function HotelFacility() {
   }, []);
 
   return (
-    <Form
-      form={form}
-      labelCol={{
-        span: 24,
-      }}
-      layout="vertical"
-      name="basic"
-      onFinish={onReportFormFinish}
-      style={{
-        minHeight: 'calc(100vh - 209px)',
-      }}
-      wrapperCol={{
-        span: 23,
-      }}
-    >
-      <Card bordered={false} size="small">
-        <Row>
-          <Col span={8}>
-            <Form.Item
-              label={t('report.Branch.title')}
-              name="branch_id"
-              rules={[{ required: true, message: 'Please select branch' }]}
-            >
-              <Select
-                allowClear
-                onChange={changeBranch}
-                placeholder={t('report.Branch.placeholder')}
+    <Spin spinning={isLoading}>
+      <Form
+        form={form}
+        labelCol={{
+          span: 24,
+        }}
+        layout="vertical"
+        name="basic"
+        onFinish={onReportFormFinish}
+        style={{
+          minHeight: 'calc(100vh - 209px)',
+        }}
+        wrapperCol={{
+          span: 23,
+        }}
+      >
+        <Card bordered={false} size="small">
+          <Row>
+            <Col span={8}>
+              <Form.Item
+                label={t('report.Branch.title')}
+                name="branch_id"
+                rules={[{ required: true, message: 'Please select branch' }]}
               >
-                {branchs.length > 0 &&
-                  branchs.map((branch: any) => <Option key={branch.id}>{branch.name}</Option>)}
-              </Select>
-            </Form.Item>
+                <Select
+                  allowClear
+                  onChange={changeBranch}
+                  placeholder={t('report.Branch.placeholder')}
+                >
+                  {branchs.length > 0 &&
+                    branchs.map((branch: any) => <Option key={branch.id}>{branch.name}</Option>)}
+                </Select>
+              </Form.Item>
 
-            <Form.Item
-              label={t('report.Facility.title')}
-              name="facility_id"
-              rules={[{ required: true, message: 'Please select facility' }]}
-            >
-              <Select allowClear placeholder={t('report.Facility.placeholder')}>
-                {facilities.length > 0 &&
-                  facilities.map((facility: any) => (
-                    <Option key={facility.id} value={facility.id.toString()}>
-                      {facility.name}
-                    </Option>
-                  ))}
-              </Select>
-            </Form.Item>
+              <Form.Item
+                label={t('report.Facility.title')}
+                name="facility_id"
+                rules={[{ required: true, message: 'Please select facility' }]}
+              >
+                <Select allowClear placeholder={t('report.Facility.placeholder')}>
+                  {facilities.length > 0 &&
+                    facilities.map((facility: any) => (
+                      <Option key={facility.id} value={facility.id.toString()}>
+                        {facility.name}
+                      </Option>
+                    ))}
+                </Select>
+              </Form.Item>
 
-            <Form.Item label={t('report.Equipment Type.title')} name="equipment_type">
-              <Select allowClear placeholder={t('report.Equipment Type.placeholder')}>
-                {getRoomsData.items?.map((item: any) => {
-                  return <Option value={item.id}>{item.name}</Option>;
-                })}
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item
-              label={t('report.Report Type.title')}
-              name="report_type"
-              rules={[{ required: true, message: 'Please select report type' }]}
-            >
-              <Select allowClear placeholder={t('report.Report Type.placeholder')}>
-                <Option value="staying">Staying Report</Option>
-                <Option value="sales">Sale Report</Option>
-              </Select>
-            </Form.Item>
+              <Form.Item label={t('report.Equipment Type.title')} name="equipment_type">
+                <Select allowClear placeholder={t('report.Equipment Type.placeholder')}>
+                  {getRoomsData.items?.map((item: any) => {
+                    return <Option value={item.id}>{item.name}</Option>;
+                  })}
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item
+                label={t('report.Report Type.title')}
+                name="report_type"
+                rules={[{ required: true, message: 'Please select report type' }]}
+              >
+                <Select allowClear placeholder={t('report.Report Type.placeholder')}>
+                  <Option value="staying">Staying Report</Option>
+                  <Option value="sales">Sale Report</Option>
+                </Select>
+              </Form.Item>
 
-            <Form.Item label={t('report.Sort order by.title')} name="sort_order_by">
-              <Select allowClear placeholder={t('report.Sort order by.placeholder')}>
-                <Option value="checkin_date">Chekin Date</Option>
-                <Option value="checkout_date">Checkout Date</Option>
-                <Option value="equipment_type">Equipment Type</Option>
-                <Option value="folio_number">Booking ID</Option>
-              </Select>
-            </Form.Item>
+              <Form.Item label={t('report.Sort order by.title')} name="sort_order_by">
+                <Select allowClear placeholder={t('report.Sort order by.placeholder')}>
+                  <Option value="checkin_date">Chekin Date</Option>
+                  <Option value="checkout_date">Checkout Date</Option>
+                  <Option value="equipment_type">Equipment Type</Option>
+                  <Option value="folio_number">Booking ID</Option>
+                </Select>
+              </Form.Item>
 
-            <Form.Item label={t('report.Sort order type.title')} name="sort_order_type">
-              <Select allowClear placeholder={t('report.Sort order type.placeholder')}>
-                <Option value="asc">ASC</Option>
-                <Option value="desc">DESC</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item
-              label={t('report.Report Start Date.title')}
-              name="start_date"
-              rules={[{ required: true, message: 'Please select the start date' }]}
-            >
-              <DatePicker format={formatDate} style={{ width: '100%' }} />
-            </Form.Item>
-            <Form.Item label={t('report.Report End Date.title')} name="end_date">
-              <DatePicker format={formatDate} style={{ width: '100%' }} />
-            </Form.Item>
-          </Col>
-          <Col sm={8} style={{ marginBottom: 24 }}>
-            <PattonButton
-              onClick={() => form.submit()}
-              style={{
-                width: 120,
-                height: 32,
-                marginRight: 8,
-              }}
-            >
-              {t('report.Export Excel')}
-            </PattonButton>
-            <PattonButton
-              onClick={resetForm}
-              style={{
-                width: 120,
-                height: 32,
-                border: '1px solid #D9D9D9',
-                background: '#FFF',
-                color: 'rgba(0, 0, 0, 0.65)',
-              }}
-            >
-              {t('report.Reset')}
-            </PattonButton>
-          </Col>
-        </Row>
-      </Card>
-    </Form>
+              <Form.Item label={t('report.Sort order type.title')} name="sort_order_type">
+                <Select allowClear placeholder={t('report.Sort order type.placeholder')}>
+                  <Option value="asc">ASC</Option>
+                  <Option value="desc">DESC</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={8}>
+              <Form.Item
+                label={t('report.Report Start Date.title')}
+                name="start_date"
+                rules={[{ required: true, message: 'Please select the start date' }]}
+              >
+                <DatePicker format={formatDate} style={{ width: '100%' }} />
+              </Form.Item>
+              <Form.Item label={t('report.Report End Date.title')} name="end_date">
+                <DatePicker format={formatDate} style={{ width: '100%' }} />
+              </Form.Item>
+            </Col>
+            <Col sm={8} style={{ marginBottom: 24 }}>
+              <PattonButton
+                onClick={() => form.submit()}
+                style={{
+                  width: 120,
+                  height: 32,
+                  marginRight: 8,
+                }}
+              >
+                {t('report.Export Excel')}
+              </PattonButton>
+              <PattonButton
+                onClick={resetForm}
+                style={{
+                  width: 120,
+                  height: 32,
+                  border: '1px solid #D9D9D9',
+                  background: '#FFF',
+                  color: 'rgba(0, 0, 0, 0.65)',
+                }}
+              >
+                {t('report.Reset')}
+              </PattonButton>
+            </Col>
+          </Row>
+        </Card>
+      </Form>
+    </Spin>
   );
 }
 
