29683d080300fcfc135c9b026ababeeefbed4206
minhnv2306 <minhnv2306@gmail.com>
2023-09-26 22:43:02 +0700

    feat: integrate API for financial info

--- src/pages/reservation/component/ReservationDetailTab/FinancialDetail.tsx
+++ src/pages/reservation/component/ReservationDetailTab/FinancialDetail.tsx
@@ -8,57 +8,58 @@ Main functions : Financial Detail
 
 import React from 'react';
 import { Col, Table } from 'antd';
+import { formatNumber } from 'helpers';
+import moment from 'moment';
 import useColumns from 'pages/reservation/create/useColumns';
+import { selectGetReservation } from 'selectors';
 
-const financialDetailDataSample = [
-  {
-    date: '22/07/2023',
-    description: 'Phong Junior Deluxe Double Traveloka',
-    gross_revenue: '1,012,500',
-    ta_comp: '151,875',
-    payment_method: 'VCC',
-    amount: '860,625',
-    system_fee: '26,417',
-  },
-  {
-    date: '22/07/2023',
-    description: 'Phong Junior Deluxe Double Traveloka',
-    gross_revenue: '1,012,500',
-    ta_comp: '151,875',
-    payment_method: 'VCC',
-    amount: '860,625',
-    system_fee: '26,417',
-  },
-  {
-    date: '22/07/2023',
-    description: 'Thue xe',
-    gross_revenue: '500,000',
-    ta_comp: '0',
-    payment_method: 'VNPAY',
-    amount: '500,000',
-    system_fee: '16,500',
-  },
-];
+import { useAppSelector } from 'modules/hooks';
+
+type FinancialItem = {
+  date: string;
+  description: string;
+  gross_revenue: number;
+  payment_method: string;
+  payment_system_fee: number;
+  ta_comp: number;
+  transaction_amount: number;
+};
 
 function FinancialDetail() {
   const { financialDetailColumns } = useColumns();
+  const { data: reservationInfo } = useAppSelector(selectGetReservation);
+
+  const financialData = reservationInfo.financial_details.map((item: FinancialItem) => {
+    return {
+      date: moment(item.date).format('DD/MM/YYYY'),
+      description: item.description,
+      gross_revenue: formatNumber(item.gross_revenue),
+      ta_comp: formatNumber(item.ta_comp),
+      payment_method: item.payment_method,
+      amount: formatNumber(item.transaction_amount),
+      system_fee: formatNumber(item.payment_system_fee),
+    };
+  });
 
   return (
     <Col span={24} style={{ marginTop: 5, marginBottom: 15 }}>
       <Table
         columns={financialDetailColumns}
-        dataSource={financialDetailDataSample}
+        dataSource={financialData}
         pagination={false}
+        scroll={{ y: 240 }}
         size="small"
         summary={() => {
           return (
-            <>
+            <Table.Summary fixed>
               <Table.Summary.Row style={{ fontWeight: 'bold', color: 'rgba(0, 0, 0, 0.65)' }}>
                 <Table.Summary.Cell colSpan={6} index={0}>
                   <div style={{ paddingLeft: 20 }}>Total Revenue</div>
                 </Table.Summary.Cell>
                 <Table.Summary.Cell index={1}>
-                  <div style={{ textAlign: 'right', paddingRight: 20 }}>2,525,000</div>
+                  <div style={{ textAlign: 'right', paddingRight: 20 }}>
+                    {formatNumber(reservationInfo.financial_details_summary.total_revenue)}
+                  </div>
                 </Table.Summary.Cell>
               </Table.Summary.Row>
               <Table.Summary.Row style={{ fontWeight: 'bold', color: 'rgba(0, 0, 0, 0.65)' }}>
@@ -68,7 +69,9 @@ function FinancialDetail() {
                 </Table.Summary.Cell>
 
                 <Table.Summary.Cell index={1}>
-                  <div style={{ textAlign: 'right', paddingRight: 20 }}>303,750</div>
+                  <div style={{ textAlign: 'right', paddingRight: 20 }}>
+                    {formatNumber(reservationInfo.financial_details_summary.total_tacomp)}
+                  </div>
                 </Table.Summary.Cell>
               </Table.Summary.Row>
               <Table.Summary.Row style={{ fontWeight: 'bold', color: 'rgba(0, 0, 0, 0.65)' }}>
@@ -77,34 +80,37 @@ function FinancialDetail() {
                 </Table.Summary.Cell>
 
                 <Table.Summary.Cell index={1}>
-                  <div style={{ textAlign: 'right', paddingRight: 20 }}>57,234</div>
+                  <div style={{ textAlign: 'right', paddingRight: 20 }}>
+                    {formatNumber(reservationInfo.financial_details_summary.total_system_fee)}
+                  </div>
                 </Table.Summary.Cell>
               </Table.Summary.Row>
-              <Table.Summary.Row style={{ color: 'rgba(0, 0, 0, 0.65)' }}>
-                <Table.Summary.Cell colSpan={6} index={0}>
-                  <div style={{ paddingLeft: 20 }}>Payment System Fee (VCC)</div>
-                </Table.Summary.Cell>
-                <Table.Summary.Cell index={1}>
-                  <div style={{ textAlign: 'right', paddingRight: 20 }}>52,834</div>
-                </Table.Summary.Cell>
-              </Table.Summary.Row>
-              <Table.Summary.Row style={{ color: 'rgba(0, 0, 0, 0.65)' }}>
-                <Table.Summary.Cell colSpan={6} index={0}>
-                  <div style={{ paddingLeft: 20 }}>Payment System Fee (VNPAY)</div>
-                </Table.Summary.Cell>
-                <Table.Summary.Cell index={1}>
-                  <div style={{ textAlign: 'right', paddingRight: 20 }}>4,400</div>
-                </Table.Summary.Cell>
-              </Table.Summary.Row>
+              {reservationInfo.financial_details_summary.tacomp_details.map((item: any) => (
+                <Table.Summary.Row style={{ color: 'rgba(0, 0, 0, 0.65)' }}>
+                  <Table.Summary.Cell colSpan={6} index={0}>
+                    <div style={{ paddingLeft: 20 }}>
+                      Payment System Fee ({item.payment_method})
+                    </div>
+                  </Table.Summary.Cell>
+                  <Table.Summary.Cell index={1}>
+                    <div style={{ textAlign: 'right', paddingRight: 20 }}>
+                      {formatNumber(item.payment_system_fee)}
+                    </div>
+                  </Table.Summary.Cell>
+                </Table.Summary.Row>
+              ))}
+
               <Table.Summary.Row style={{ fontWeight: 'bold', color: 'rgba(0, 0, 0, 0.65)' }}>
                 <Table.Summary.Cell colSpan={6} index={0}>
                   <div style={{ paddingLeft: 20 }}>Net Revenue</div>{' '}
                 </Table.Summary.Cell>
                 <Table.Summary.Cell index={2}>
-                  <div style={{ textAlign: 'right', paddingRight: 20 }}>1,664,016</div>
+                  <div style={{ textAlign: 'right', paddingRight: 20 }}>
+                    {formatNumber(reservationInfo.financial_details_summary.total_net_revenue)}
+                  </div>
                 </Table.Summary.Cell>
               </Table.Summary.Row>
-            </>
+            </Table.Summary>
           );
         }}
       />
