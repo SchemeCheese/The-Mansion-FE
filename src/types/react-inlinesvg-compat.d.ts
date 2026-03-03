a4f1d8e94acd3e8b34123fa98f0b14ad2374c13d
Steve Vu <du.vu@minova.vn>
2026-03-03 23:50:09 +0700

    chore(frontend): upgrade svg and helmet deps for react18
    - bump react-inlinesvg to v4 and react-helmet-async to latest v1
    
    - pin react-qr-reader version instead of wildcard for deterministic installs
    
    - add local type shim for react-inlinesvg to keep TS compatibility

---
+++ src/types/react-inlinesvg-compat.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable react/no-typos */
+declare module 'react-inlinesvg' {
+  import * as React from 'react';
+
+  const InlineSVG: React.ComponentType<any>;
+
+  export default InlineSVG;
+}
